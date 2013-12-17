7ff3ff073ed400496c2709e0fd311097c475a430
Aditya <aditya@netroy.in>
2013-12-17 23:42:19 +0100

    grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-complexity');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('kaapi');
+
+  function config (name) {
+    return grunt.file.readJSON('grunt/configs/' + name + '.json');
+  }
+
+  grunt.initConfig({
+
+    // Linting
+    'jshint': config('jshint'),
+    'complexity': config('complexity'),
+
+    // JS Build
+    'requirejs': config('requirejs'),
+    'uglify': config('uglify'),
+
+    // Specs
+    'kaapi/node': config('kaapi'),
+    'kaapi/phantom': config('kaapi')
+  });
+};
