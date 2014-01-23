0acad242f9da77c54d69f9d066d22bc431d000e0
Aditya <aditya@netroy.in>
2014-01-23 14:07:25 +0100

    moar specs for libs stuff

--- public/lib/clone.js
+++ public/lib/clone.js
@@ -8,11 +8,22 @@ define(function () {
     return arr.slice();
   }
 
+  function cloneDate (date) {
+    return new Date(date);
+  }
+
   function cloneObject (source) {
     var object = {};
     for (var key in source) {
       if (source.hasOwnProperty(key)) {
-        object[key] = source[key];
+        var value = source[key];
+        if (value instanceof Date) {
+          object[key] = cloneDate(value);
+        } else if (typeof value === 'object' && value !== null) {
+          object[key] = clone(value);
+        } else {
+          object[key] = value;
+        }
       }
     }
     return object;
