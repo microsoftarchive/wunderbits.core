89d7fac0ac9b750e097b945ce287c4f43a48aaa5
Aditya <aditya@netroy.in>
2014-01-27 17:56:29 +0100

    add support for pre-init properties on mixins

--- public/WBMixin.js
+++ public/WBMixin.js
@@ -22,11 +22,27 @@ define([
         delete behavior.initialize;
       }
 
+      // augment mixin's properties object into the instance
+      var properties = {};
+      if (behavior.properties) {
+        properties = behavior.properties;
+        delete behavior.properties;
+      }
+
       // mixin the behavior
       extend(instance, behavior);
 
       // apply the initializer, if any
       initializer && initializer.apply(instance);
+
+      for (var key in properties) {
+        if (properties.hasOwnProperty(key)) {
+          if (instance[key]) {
+            throw new Error('"' + key + '" already exists on the instance');
+          }
+          instance[key] = clone(properties[key]);
+        }
+      }
 
       return instance;
     },
@@ -47,6 +63,18 @@ define([
         var initializers = proto.initializers = proto.initializers || [];
         initializers.push(initialize);
         delete behavior.initialize;
+      }
+
+      // cache the properties, to be applied later
+      var props = proto.properties = proto.properties || {};
+      var properties = behavior.properties || {};
+      for (var key in properties) {
+        if (properties.hasOwnProperty(key)) {
+          if (proto[key]) {
+            throw new Error('"' + key + '" already exists on the prototype');
+          }
+          props[key] = clone(properties[key]);
+        }
       }
 
       // extend the prototype
