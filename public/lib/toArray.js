663c2a7c701c773c19ee314a3b56440a7d5b135d
Raymond May Jr <raymondmay@gmail.com>
2014-05-20 15:42:54 +0200

    alternate method for args -> array
    http://jsperf.com/arguments-to-array-doch

--- public/lib/toArray.js
+++ public/lib/toArray.js
@@ -1,8 +1,18 @@
 'use strict';
 
-var slice = Array.prototype.slice;
-function toArray (obj, skip) {
-  return slice.call(obj, skip || 0);
+function toArray (arrayLikeObj, skip) {
+
+  skip = skip || 0;
+
+  var length = arrayLikeObj.length;
+  var arrLength = length - skip;
+  var arr = new Array(arrLength > 0 ? arrLength : 0);
+
+  for (var i = skip; i < length; i++) {
+    arr[i - skip] = arrayLikeObj[i];
+  }
+
+  return arr;
 }
 
 module.exports = toArray;
