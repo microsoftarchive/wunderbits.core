3f6fa011613cb7fc45e5cf9a3d4f8632703a7c1e
Aditya <aditya@netroy.in>
2014-05-23 16:45:31 +0200

    lower complexity

--- public/lib/createUID.js
+++ public/lib/createUID.js
@@ -1,25 +1,44 @@
-/*jslint maxlen: 150 */
 // http://stackoverflow.com/a/21963136/933653
 'use strict';
 
+var ff = 0xff;
 var lut = [];
 for (var i = 0; i < 256; i++) {
   lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
 }
 
+var random = Math.random;
+function randHex() {
+  return (random() * 0xffffffff | 0);
+}
+
+function section0 () {
+  var d0 = randHex();
+  return lut[d0 & ff] + lut[d0 >> 8 & ff] +
+           lut[d0 >> 16 & ff] + lut[d0 >> 24 & ff];
+}
+
+function section1 () {
+  var d1 = randHex();
+  return lut[d1 & ff] + lut[d1 >> 8 & ff] + '-' +
+         lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & ff];
+}
+
+function section2 () {
+  var d2 = randHex();
+  return lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & ff] + '-' +
+       lut[d2 >> 16 & ff] + lut[d2 >> 24 & ff];
+}
+
+function section3 () {
+  var d3 = randHex();
+  return lut[d3 & ff] + lut[d3 >> 8 & ff] +
+       lut[d3 >> 16 & ff] + lut[d3 >> 24 & ff];
+}
+
 function createUID (prefix) {
-
-  var d0 = Math.random() * 0xffffffff | 0;
-  var d1 = Math.random() * 0xffffffff | 0;
-  var d2 = Math.random() * 0xffffffff | 0;
-  var d3 = Math.random() * 0xffffffff | 0;
-
-  var uid = lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
-    lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
-    lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
-    lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
-
-  return String(!prefix ? '' : prefix) + uid;
+  var uid = [section0(), section1(), section2(), section3()].join('-');
+  return (!prefix ? '' : prefix).toString() + uid;
 }
 
 module.exports = createUID;
