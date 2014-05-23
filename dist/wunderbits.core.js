0698f8b3a2bb953fe0bcde4c57d6c6535c38aff0
Raymond May Jr <raymondmay@gmail.com>
2014-05-23 15:30:26 +0200

    MAKE IT LIGHTSPEED
    http://jsperf.com/creauid

--- dist/wunderbits.core.js
+++ dist/wunderbits.core.js
@@ -665,21 +665,31 @@ function clone (obj, isDeep) {
 module.exports = clone;
 
 },{}],13:[function(_dereq_,module,exports){
+/*jslint maxlen: 150 */
+// http://stackoverflow.com/a/21963136/933653
 'use strict';
 
-function replacer (match) {
-  var rand = Math.random() * 16 | 0;
-  var chr = (match === 'x') ? rand : (rand & 0x3 | 0x8);
-  return chr.toString(16);
+var lut = [];
+for (var i = 0; i < 256; i++) {
+  lut[i] = (i < 16 ? '0' : '') + (i).toString(16);
 }
 
 function createUID (prefix) {
-  var uid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, replacer);
+
+  var d0 = Math.random() * 0xffffffff | 0;
+  var d1 = Math.random() * 0xffffffff | 0;
+  var d2 = Math.random() * 0xffffffff | 0;
+  var d3 = Math.random() * 0xffffffff | 0;
+
+  var uid = lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
+    lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
+    lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
+    lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
+
   return String(!prefix ? '' : prefix) + uid;
 }
 
 module.exports = createUID;
-
 },{}],14:[function(_dereq_,module,exports){
 'use strict';
 
--- tests/lib/createUID.spec.js
+++ tests/lib/createUID.spec.js
@@ -7,4 +7,25 @@ describe('createUID', function () {
   it('should be a function', function () {
     expect(topic).to.be.a('function');
   });
+
+  it('should generate unique uuids', function () {
+
+    var ids = [];
+
+    var id;
+    for (var i=0, len=10000; i <= len; i++) {
+      id = topic();
+      expect(ids.indexOf(id)).to.equal(-1);
+      ids.push(id);
+    }
+  });
+
+  it ('should generate a uuid with a prefix', function () {
+
+    var str = 'foobar';
+    var regex = new RegExp('^' + str);
+
+    var uuid = topic(str);
+    expect(regex.test(uuid)).to.be.true;
+  });
 });
